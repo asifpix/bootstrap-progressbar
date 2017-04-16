0befa105d53fb531ef3d8b75531070c85c994853
Asif Islam <asifpix@gmail.com>
2017-04-16 19:08:38 +0600

    init

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function($){
+	"use strict";
+	//when dom is ready start
+	$(document).ready(function(){
+
+		$('.progress-bar').each(function(){
+
+            var width = $(this).data('percent');
+            $(this).css({'transition': 'width 3s'});
+
+            $(this).appear(function() {
+                console.log('hello');
+                $(this).css('width', width + '%');
+                $(this).find('.count').countTo({
+                    from: 0,
+                    to: width,
+                    speed: 3000,
+                    refreshInterval: 50,
+                });
+            });
+
+        });
+
+	});
+	//when dom is ready end
+})(jQuery);
